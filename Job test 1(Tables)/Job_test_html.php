ed8e44951a8e7418d1c38ba369e74f557b09632d
Ashutoshbht <ashutosh@codedrillinfotech.com>
2024-05-10 17:34:35 +0530

    Add files via upload

---
+++ Job test 1(Tables)/Job_test_html.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- TOP NAV BAR -->
+    <table border="0" color cellpadding="10" width="100%" cellspacing="0">
+        <tr bgcolor="blue" color="white">
+            <td>
+                <h3><i>My Beautiful new website</i></h3>
+                <p><sub>Here you find all information you need</sub></p>
+            </td>
+            <td bgcolor="green" color="blue" height="200" align="center">
+                <p>Beispielgrafix</p>
+            </td>
+        </tr>
+    </table>
+
+    <!-- LINK NAV BAR -->
+<table border="1" bgcolor="green" width="100%" cellspacing="0">
+    <tr>
+        <td colspan="50">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+        <td align="center" color="white">Link 1</td>
+        <td align="center" color="white">Link 2</td>
+        <td align="center" color="white">Link 3</td>
+        <td align="center" color="white">Link 4</td>
+        <td align="center" color="white">Link 7</td>
+        <td align="center" color="white">Link 6</td>
+        <td align="center" color="white">Link 7</td>
+        <td colspan="50">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+    </tr>
+</table><br>
+
+
+    <!-- LEFT SECTION TABLE -->
+    <table border="0" bgcolor="green" color="white" width="30%" align="left">
+        <tr>
+            <td align="center" colspan="2">Title 1</td>
+        </tr>
+    </table> <br> <br><br>
+        <!-- left second table -->
+        <table border="1" bgcolor="blue" color="white" width="30%" align="left">
+            <tr>
+                <td align="left" colspan="2">&gt;&gt;Home</td>
+            </tr>
+            <tr>
+                <td align="left" colspan="2">&gt;&gt;Page 2</td>
+            </tr>
+            <tr>
+                <td align="left" colspan="2">&gt;&gt;Page 3</td>
+            </tr>
+            <tr>
+                <td align="left" colspan="2">&gt;&gt;Page 4</td>
+            </tr>
+            <tr>
+                <td align="left" colspan="2">&gt;&gt;Page 5</td>
+            </tr>
+            <tr>
+                <td align="left" colspan="2">&gt;&gt;Page 6</td>
+            </tr>
+        </table>
+
+    <!-- RIGHT SECTION TABLE -->
+    <table border="0" bgcolor="transparent" width="70%" align="right">
+        <tr>
+            <td align="center">
+                <h3>H1-HEADING 1</h3>
+                <p align="justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat id debitis ad
+                    maiores assumenda porro animi optio obcaecati magnam temporibus, omnis voluptate natus quasi illum
+                    necessitatibus suscipit esse eveniet voluptatibus corrupti ut accusantium quis. Fugiat ducimus earum
+                    doloremque quidem tempore aperiam, ab necessitatibus dicta voluptatibus dolores aliquam, vitae
+                    cupiditate eum?</p>
+            </td>
+        </tr>
+    </table>
+    <!-- Right section below H1 / H2 HEADING -->
+    <table border="0" bgcolor="transparent" width="70%" align="right">
+        <tr>
+            <td align="left">
+                <h3>H2-HEADING </h3>
+                <p align="justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat id debitis ad
+                    maiores assumenda porro animi optio obcaecati magnam temporibus, omnis voluptate natus quasi illum
+                    necessitatibus suscipit esse eveniet voluptatibus corrupti ut accusantium quis. Fugiat ducimus earum
+                    doloremque quidem tempore aperiam, ab necessitatibus dicta voluptatibus dolores aliquam, vitae
+                    cupiditate eum?</p>
+            </td>
+        </tr>
+    </table>
+    <!-- H3 HEADING -->
+    <table border="0" bgcolor="transparent" width="70%" align="right">
+        <tr>
+            <td align="center">
+                <h3><i>H3-HEADING </i></h3>
+                <p align="justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat id debitis ad
+                    maiores assumenda porro animi optio obcaecati magnam temporibus, omnis voluptate natus quasi illum
+                    necessitatibus suscipit esse eveniet voluptatibus corrupti ut accusantium quis. Fugiat ducimus earum
+                    doloremque quidem tempore aperiam, ab necessitatibus dicta voluptatibus dolores aliquam, vitae
+                    cupiditate eum?</p>
+            </td>
+        </tr>
+    </table>
+    <!-- H4 HEADING -->
+    <table border="0" bgcolor="green" width="70%" align="Right" cellpadding="5" cellspacing="0">
+        <tr>
+            <td color="white"> 
+                <h3>H4-Heading</h3>
+            </td>
+        </tr>
+    </table>
+    <table border="0" width="70%" align="Right" >
+        <tr>
+            <td>
+                <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo asperiores praesentium non quibusdam odio, veritatis nobis, consectetur error pariatur velit odit illo quisquam magni facilis, omnis veniam aliquid consequuntur ex?</P>
+            </td>
+        </tr>
+    </table>
+    
+            <!-- H5 HEADING -->
+    <table border="0" bgcolor="transparent" width="70%" align="right">
+        <tr>
+            <td align="LEFT">
+                <H4 color="green">H5-Heading</H4> 
+                <p align="justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat id debitis ad
+                    maiores assumenda porro animi optio obcaecati magnam temporibus, omnis voluptate natus quasi illum
+                    necessitatibus suscipit esse eveniet voluptatibus corrupti ut accusantium quis. Fugiat ducimus earum
+                    doloremque quidem tempore aperiam, ab necessitatibus dicta voluptatibus dolores aliquam, vitae
+                    cupiditate eum?</p>
+            </td>
+    
+        </tr>
+    
+    </table>
+    <!-- Horizontal line -->
+    <table border="0" bgcolor="transparent" width="70%" align="right">
+        <tr>
+            <td>
+                <!-- HR is used to draw a line horizontally -->
+                <hr>  
+            </td>
+        </tr>
+    </table>
+
+    <!-- footer table -->
+    <table border="0" bgcolor="green" width="70%" align="right" cellpadding="0" cellspacing="0">
+        <tr>
+            <td>
+                <h4>This template is linkware</h4>
+            </td>
+        </tr>
+    </table>
+    <table border="0" bgcolor="transparent" width="70%" align="right">
+        <tr>
+            <td>
+            <h4>    That means: </h4> <br>
+            <li> Lorem ipsum dolor sit amet consectetur adipisicing elit. <strong>terms for free use</strong>Assumenda cum veritatis est? Explicabo, numquam! Eveniet delectus beatae totam odio laboriosam.</li>
+            </td>
+        </tr>
+    </table>
+
+
+
+
+
+
+
+</body>
+
+</html>
